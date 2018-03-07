209d1bb82c68915e8c1d30c8f29b606d38596f4b
AdanMora <ajmorafallas@gmail.com>
2018-03-06 18:11:33 -0600

    Plot y conversión RGBtoGrayscale

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 import numpy as np
+import pickle
+
+def unpickle(file):
+    """Función para descomprimir los archivos de imágenes de CIFAR-10"""
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def RGBtoGrayscale(img):
+    lenV = img.size // 3
+    resultado = []
+    R = img[:1024]
+    img = img[1024:]
+    G = img[:1024]
+    B = img[1024:]
+    for i in range(lenV):
+        resultado.append((R[0]*0.299) + (G[0]*0.587) + (B[0]*0.114))
+        R = R[1:]
+        G = G[1:]
+        B = B[1:]
+    return np.array(resultado, dtype= np.float32)
+        
+def plotImage(img):
+    from pylab import imshow, show, get_cmap
+
+    ind = 0
+    m = []
+    for i in range(32):
+        f = []
+        for j in range(32):
+            f.append(img[ind])
+            ind += 1
+        m.append(f)
+
+    m = np.array(m)
+    imshow(m, cmap="gray")
+    show()
 
 def hingeLoss():
 	pass
-	
 
 def main():
 	pass
