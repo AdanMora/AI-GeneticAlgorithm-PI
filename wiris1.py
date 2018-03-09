24a00f21ed9900b94c15399987c05498a4f1f3b4
Iván Calvo Pérez <ivanfelipecp@gmail.com>
2018-03-09 11:13:27 -0600

    + Nueva librería

---
+++ wiris1.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+tipo = np.dtype([("w",np.float32,(3,4)),("L",np.float32),("Li",np.float32,(3,))])
+
+def generateRoW():
+    #values = iris_values[flower]
+    minimo = 0.0
+    maximo = 10.0
+    sepal_length = np.random.uniform(low=minimo,high=maximo,size=(1,))
+    sepal_width = np.random.uniform(low=minimo,high=maximo,size=(1,))
+    petal_length = np.random.uniform(low=minimo,high=maximo,size=(1,))
+    petal_width = np.random.uniform(low=minimo,high=maximo,size=(1,))
+
+    return np.concatenate((sepal_length,sepal_width,petal_length,petal_width))
+
+def generateW():
+    row1 = generateRoW()
+    row2 = generateRoW()
+    row3 = generateRoW()
+
+    return np.array([row1,row2,row3])
+
+def generateWs(n):
+    ws = []
+    for i in range(n):
+        ws.append((generateW(),0,[0,0,0]))
+    return np.array(ws,dtype=tipo)
+
+def generateIndividuos(ws):
+    return np.array(ws,dtype=tipo)
